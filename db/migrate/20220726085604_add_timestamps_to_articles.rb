f08b6bda9f442cd971b1b0d9a3464ec9fa101d9c
Marcos Andaluz de Rosas <marcos.andaluz@factorial.co>
2022-08-09 09:51:36 +0200

    adding validations to title and description

--- db/migrate/20220726085604_add_timestamps_to_articles.rb
+++
@@ -1,7 +0,0 @@
-class AddTimestampsToArticles < ActiveRecord::Migration[7.0]
-  def change
-    add_column :articles, :created_at, :datetime
-    add_column :articles, :update_at, :datetime
-
-  end
-end
